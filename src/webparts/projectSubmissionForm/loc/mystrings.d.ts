ec7c15bce81f46a99cb4874f27b8b1fbc1b6437f
Scout Martinelli <scout.martinelli@siriuscom.com>
2017-10-30 08:58:51 -0500

    first commit

---
+++ src/webparts/projectSubmissionForm/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IProjectSubmissionFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ProjectSubmissionFormWebPartStrings' {
+  const strings: IProjectSubmissionFormWebPartStrings;
+  export = strings;
+}
